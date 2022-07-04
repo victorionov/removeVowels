545d01c1a000107d6a5205efb5f84c309ef6e513
Viktor Ionov <victorionovcvlrs@gmail.com>
2022-07-04 13:38:28 +0200

    Working

---
+++ RemoveVowels.js
@@ -0,0 +1,13 @@
+function removeVowels(doc) {
+    let vow = "aeiouy";
+    let result = "";
+    for (let letter of doc) {
+       let lowerKaseLetter = letter.toLowerCase();
+       if (!vow.includes(lowerKaseLetter)) {
+           result += lowerKaseLetter;
+       }
+    }
+    return result;
+   }
+   removeVowels("qnokjJSLAADOUpsuidfnmw");
+   
